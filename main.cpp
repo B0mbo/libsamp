668c1c9ace3a2097494ab5bec68ed94b64bea4d0
user <user>
2023-12-17 15:21:07 -0500

    Update: add functions show_server and show_players

--- main.cpp
+++ main.cpp
@@ -6,23 +6,42 @@
 
 using namespace sampdata;
 
+void show_server(LibSamp& lsSamp, std::string& strAddr, int nPort);
+void show_players(LibSamp& lsSamp, std::string& strAddr, int nPort);
+
 int main(int argc, char *argv[])
 {
     LibSamp lsSamp;
     Samp_i_Response sir;
     Samp_d_Response sdr;
-    std::string strAddr = std::string("46.174.50.164");
+    std::string strAddr = std::string("191.96.53.178");
 
     if(argc > 1)
     {
 	if (strcmp("players", argv[1]) == 0) {
-	    lsSamp.getPlayersList(strAddr, 7777, sdr);
+	    show_players(lsSamp, strAddr, 7797);
+	}
+
+	if (strcmp("server", argv[1]) == 0) {
+	    show_server(lsSamp, strAddr, 7797);
+	}
+    } else {
+	show_server(lsSamp, strAddr, 7797);
+	show_players(lsSamp, strAddr, 7797);
+    }
+    return 0;
+}
+
+void show_players(LibSamp& lsSamp, std::string& strAddr, int nPort)
+{
+	    Samp_i_Response sir;
+	    Samp_d_Response sdr;
+	    lsSamp.getPlayersList(strAddr, nPort, sdr);
 
 	    for(int nIndex = 0; nIndex < sdr.vPlayers.size(); ++nIndex)
 	    {
 		std::cout << sdr.vPlayers.at(nIndex).strPlayerName << std::endl;
 	    }
-
 //	    for(int nIndex = 0; nIndex < sdr.vPlayers.size(); ++nIndex)
 //	    {
 //		Samp_d_Response::PlayerData pd = sdr.vPlayers.at(nIndex);
@@ -31,12 +50,12 @@ int main(int argc, char *argv[])
 //		else
 //		    std::cout << (int)pd.cPlayerID << "\t" << pd.strPlayerName  << "\t\t" << pd.nPlayerScore << "\t" << pd.nPlayerPing << std::endl;
 //	    }
-	}
+}
 
-	if (strcmp("server", argv[1]) == 0) {
-	    lsSamp.getServerInfo(strAddr, 7777, sir);
-	    std::cout << sir.strHostName << "\t" << (int)sir.sPlayers << "/" << sir.sMaxPlayers << std::endl;
-	}
-    }
-    return 0;
+void show_server(LibSamp& lsSamp, std::string& strAddr, int nPort)
+{
+	    Samp_i_Response sir;
+	    Samp_d_Response sdr;
+	    lsSamp.getServerInfo(strAddr, nPort, sir);
+	    std::cout << sir.strHostName << "\t" << (int)sir.sPlayers << "/" << sir.sMaxPlayers << std::endl;    
 }
