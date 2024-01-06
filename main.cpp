8f13309cf4f952c3a704546de2d72b5b114ff61e
B0mbo <chaticq@bk.ru>
2024-01-06 07:20:50 +0300

    Update: -hardcode, +default value

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 
 #include "LibSamp.h"
 
+#define SERVER_ADDR "191.96.53.178"
+#define SERVER_PORT 7797
+
+//#define SERVER_ADDR "46.174.50.164"
+//#define SERVER_PORT 7777
+
 using namespace sampdata;
 
 int main(int argc, char *argv[])
@@ -11,12 +17,12 @@ int main(int argc, char *argv[])
     LibSamp lsSamp;
     Samp_i_Response sir;
     Samp_d_Response sdr;
-    std::string strAddr = std::string("46.174.50.164");
+    std::string strAddr = std::string(SERVER_ADDR);
 
     if(argc > 1)
     {
 	if (strcmp("players", argv[1]) == 0) {
-	    lsSamp.getPlayersList(strAddr, 7777, sdr);
+	    lsSamp.getPlayersList(strAddr, SERVER_PORT, sdr);
 
 	    for(int nIndex = 0; nIndex < sdr.vPlayers.size(); ++nIndex)
 	    {
@@ -34,9 +40,17 @@ int main(int argc, char *argv[])
 	}
 
 	if (strcmp("server", argv[1]) == 0) {
-	    lsSamp.getServerInfo(strAddr, 7777, sir);
+	    lsSamp.getServerInfo(strAddr, SERVER_PORT, sir);
 	    std::cout << sir.strHostName << "\t" << (int)sir.sPlayers << "/" << sir.sMaxPlayers << std::endl;
 	}
+    } else {
+	    lsSamp.getServerInfo(strAddr, SERVER_PORT, sir);
+	    std::cout << sir.strHostName << "\t" << (int)sir.sPlayers << "/" << sir.sMaxPlayers << std::endl;
+	    lsSamp.getPlayersList(strAddr, SERVER_PORT, sdr);
+	    for(int nIndex = 0; nIndex < sdr.vPlayers.size(); ++nIndex)
+	    {
+		std::cout << sdr.vPlayers.at(nIndex).strPlayerName << std::endl;
+	    }
     }
     return 0;
 }
